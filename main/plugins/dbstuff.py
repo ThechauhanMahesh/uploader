e8e59ae4c266ec1846ae8f86956eef80d4a9b337
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-09 01:19:17 +0530

    Update dbstuff.py

--- main/plugins/dbstuff.py
+++ main/plugins/dbstuff.py
@@ -111,32 +111,7 @@ async def bcast(event):
                      [Button.inline(f"SENT: {len(sent)}", data="none")],
                      [Button.inline(f"FAILED: {len(failed)}", data="none")]])
     
-    
-#Listing--------------------------------------------------------------------------------------------------------------
 
-#Not in use
-def one_trial_queue(id, List1):
-    if f'{id}' in List1:
-        return False
-    
-#Not in use
-def two_trial_queue(id, List1, List2):
-    if not f'{id}' in List1:
-        List1.append(f'{id}')
-    else:
-        if not f'{id}' in List2:
-            List2.append(f'{id}')
-        else:
-            return False
-
-#Not in use        
-def ps_queue(id, media, List1, List2):
-    List1.append(f'{id}')
-    List2.append(media)
-    if not len(List1) < 2:
-        return 'EMPTY'
-    if len(List1) > 2:
-        return 'FULL'
 
     
 
