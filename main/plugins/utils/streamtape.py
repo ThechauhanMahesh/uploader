fed0948e733b16b1559b7ea3fcdd335759cd8d2f
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-09 15:14:02 +0530

    Update streamtape.py

--- main/plugins/utils/streamtape.py
+++ main/plugins/utils/streamtape.py
@@ -16,3 +16,16 @@ def upload(login, key, file_path):
     upload_response = upload_request.json()
     return upload_response 
     #status: 200 => Success
+
+""" response
+
+{'status': 200, 
+ 'msg': 'Upload finished!', 
+ 'result': {'url': 'https://streamtape.com/v/V0aVPA9gegIKV6g/x_%281%29.mp4', 
+            'sha256': 'e89791cd2bc63a33ffb2a617c660296e137be2466f20d74d77d306d4490c9937', 
+            'name': 'x (1).mp4', 
+            'size': '343468', 
+            'content_type': 'video/mp4', 
+            'id': 'V0aVPA9gegIKV6g'}}
+            
+            """
