f6bc7e9be9f34caec679356444f6a7c747ccd011
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-09 13:03:08 +0530

    Update mega.py

--- main/plugins/utils/mega.py
+++ main/plugins/utils/mega.py
@@ -1,7 +1,15 @@
 #tg:ChauhanMahesh/DroneBots
 #Github.com/vasusen-code
 
-
+#download mega files
+def mega_dl(url):
+    from datetime import datetime as dt
+    from mega import Mega as x
+    d = dt.now().isoformat("_", "seconds")
+    xx = x()
+    m = xx.login()
+    file = m.download_url(url)
+    return file
 
 
     
