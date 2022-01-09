97e0a3ce1f8e1c6b3a0109064244af7417d551a2
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-09 15:00:24 +0530

    Update mega.py

--- main/plugins/mega.py
+++ main/plugins/mega.py
@@ -1,13 +1,7 @@
 #tg:ChauhanMahesh/DroneBots
 #Github.com/vasusen-code
 
-from mega import Mega 
 
-#download mega files
-def mega_dl(url):
-    m = Mega().login()
-    file = m.download_url(url)
-    return file
 
 
     
