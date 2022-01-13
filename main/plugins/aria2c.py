0f27c281f7ff773eadb40b3fead5bebfb6e79fb5
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-13 23:56:16 +0530

    Update aria2c.py

--- main/plugins/aria2c.py
+++ main/plugins/aria2c.py
@@ -109,8 +109,6 @@ async def check_progress_for_dl(gid, event, edit, previous):
                     previous = msg
             else:
                 await upload_file(t_file.name, event, edit) 
-            await sleep(10)
-            await check_progress_for_dl(gid, event, edit, previous)
         except Exception as e:
             if "not found" in str(e) or "'file'" in str(e):
                 if "Your Torrent/Link is Dead." not in edit.text:
