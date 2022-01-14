e68afcd27f9308b36304794d44c1687c6c13f1e5
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-14 20:21:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
-echo "Trying to start your Bot"; 
+echo "Adding files required for aria2 config"
+wget -O /app/dht.dat https://github.com/P3TERX/aria2.conf/raw/master/dht.dat 
+wget -O /app/dht6.dat https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat 
+TRACKER=`curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt -: https://ngosang.github.io/trackerslist/trackers_all_http.txt -: https://newtrackon.com/api/all -: https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt -: https://torrends.to/torrent-tracker-list/?download=latest | awk '$1' | tr '\n' ',' | cat`
+ran=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+
+echo "Running main"
 python3 -m main
