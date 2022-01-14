dabef5efd4de09da1ddc0cef12f291dbb49d24b3
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-15 02:30:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,21 @@
-echo "Adding files required for aria2 config"
-wget -O /app/dht.dat https://github.com/P3TERX/aria2.conf/raw/master/dht.dat 
-wget -O /app/dht6.dat https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat 
-
 echo "Starting aria2c"
-tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt https://ngosang.github.io/trackerslist/trackers_all_http.txt https://newtrackon.com/api/all https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_tracker.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_exclude.txt | awk '$0' | tr '\n\n' ',')
-aria2c --enable-rpc --check-certificate=false \
-   --max-connection-per-server=10 --rpc-max-request-size=1024M --bt-max-peers=0 \
-   --bt-stop-timeout=0 --min-split-size=10M --follow-torrent=mem --split=10 \
-   --daemon=true --allow-overwrite=true --max-overall-download-limit=0 --bt-tracker="[$tracker_list]"\
-   --max-overall-upload-limit=1K --max-concurrent-downloads=15 --continue=true \
-   --peer-id-prefix=-qB4380- --user-agent=qBittorrent/4.3.8 --peer-agent=qBittorrent/4.3.8 \
-   --disk-cache=32M --bt-enable-lpd=true --seed-time=0 --max-file-not-found=0 \
-   --max-tries=20 --auto-file-renaming=true --reuse-uri=true --http-accept-gzip=true \
-   --content-disposition-default-utf8=true --netrc-path=/usr/src/app/.netrc \
-   --enable-dht=true --enable-dht6=true --dht-file-path=/app/dht.dat --dht-file-path6=/app/dht6.dat \
-   --dht-listen-port=51513 --dht-entry-point=dht.transmissionbt.com:6881 --dht-entry-point6=dht.transmissionbt.com:6881
-   
+tracker_list=$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt | awk '$0' | tr '\n\n' ',')
+aria2c --enable-rpc \
+          --rpc-listen-all=false \
+          --rpc-listen-port=6800 \
+          --max-connection-per-server=10 \
+          --rpc-max-request-size=1024M \
+          --check-certificate=false \
+          --follow-torrent=mem \
+          --seed-time=0 \
+          --max-upload-limit=1K \
+          --max-concurrent-downloads=5 \
+          --min-split-size=10M \
+          --follow-torrent=mem \
+          --split=10 \
+          --bt-tracker="[$tracker_list]" \
+          --daemon=true \
+          --allow-overwrite=true
+          
 echo "Running main"
 python3 -m main
