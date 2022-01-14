92862de3805176086f7b2f2c85d5b0348c787cd8
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-14 23:46:23 +0530

    Create ext_dl.py

---
+++ main/plugins/utils/ext_dl.py
@@ -0,0 +1,17 @@
+# (c) MaheshChauhan
+# @DroneBots; Github.com/vasusen-code
+
+#download from mediafire
+def mfdl(url):
+    output = url.split("/")[-1]
+    mediafire_dl.download(url, output, quiet=False)
+    return output
+
+#download mega files
+def mega_dl(url):
+    m = Mega().login()
+    path = f'./{dt.now().isoformat("_", "seconds")}/'
+    os.mkdir(path)
+    m.download_url(url, path) 
+    file = (os.listdir(path))[0]
+    return str(path + '/' + file)
