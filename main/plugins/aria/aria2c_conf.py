5489fde62c392e222780b82a378aaa9f6de3859e
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-15 19:05:27 +0530

    Update aria2c_conf.py

--- main/plugins/aria/aria2c_conf.py
+++ main/plugins/aria/aria2c_conf.py
@@ -1,3 +1,7 @@
-track = "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt"
+from requests import get
 
-conf = "aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port=6800 --max-connection-per-server=10 --rpc-max-request-size=1024M --check-certificate=false --follow-torrent=mem --seed-time=0 --max-upload-limit=1K --max-concurrent-downloads=5 --min-split-size=10M --follow-torrent=mem --split=10 --daemon=true --allow-overwrite=true"
+track = "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt"
+curl = get(track).text.replace("\n\n", ",")
+trackers = [f"{curl}"]
+    
+conf = f"aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port=6800 --max-connection-per-server=10 --rpc-max-request-size=1024M --check-certificate=false --follow-torrent=mem --seed-time=0 --max-upload-limit=1K --max-concurrent-downloads=5 --min-split-size=10M --follow-torrent=mem --split=10 --bt-tracker={trackers} --daemon=true --allow-overwrite=true"
