ad9aa399fa92a771d8501108ef20a386b2171ab2
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-17 17:12:06 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -18,6 +18,8 @@ FORCESUB = config("FORCESUB", default=None, cast=int)
 FORCESUB_UN = config("FORCESUB_UN", default=None)
 ACCESS_CHANNEL = config("ACCESS_CHANNEL", default=None)
 MONGODB_URI = config("MONGODB_URI", default=None)
- 
+HEROKU_API = config("HEROKU_API", default=None)
+HEROKU_APP_NAME = = config("HEROKU_APP_NAME", default=None)
+
 #Connection
 Drone = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
