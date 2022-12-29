0fc04aa93fd0447227ec0f678e34f1daa8e8475d
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-12-29 19:54:29 +0530

    Update gdown.py

--- main/plugins/gdown.py
+++ main/plugins/gdown.py
@@ -1,20 +1,33 @@
 #TG:ChauhanMahesh/DroneBots
 #Github.com/vasusen-code
 
-import re
-import os
-import time
-import gdown
-import asyncio
+import re, os, time, asyncio
+
 from .. import Drone, BOT_UN
-from telethon import events, Button
-from datetime import datetime as dt
-from ethon.telefunc import fast_upload
-from ethon.pyfunc import bash
 from LOCAL.localisation import SUPPORT_LINK
-from telethon.tl.types import MessageMediaWebPage
-from main.plugins.utils.utils import get_link, upload_folder
-from main.plugins.utils.ext_dl import get_progress
+from pathlib import Path
+from datetime import datetime as dt
+
+from ethon.pyfunc import bash
+
+from telethon import events, Button
+
+from mega import Mega
+import mediafire_dl, gdown
+
+#Downloaders-------------------------------------------------------------------------------------------------------------
+
+#download from mediafire
+def mfdl(url, id):
+    output = url.split("/")[-1]
+    mediafire_dl.download(url, output, sender_id=id, quiet=False)
+    return output
+
+#download mega files
+def mega_dl(url):
+    m = Mega().login()
+    file = m.download_url(url) 
+    return str(Path(str(file)))
 
 #to upload files from drive folder 
 #returns downloaded files path as a list
@@ -27,62 +40,32 @@ async def error(event, error, ps):
     return await event.edit(f"An error [`{error}`] occured while {ps}.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False, buttons=None)      
 
 #download files frm drive
-async def drive(event, msg):
-    folder = []
-    Drone = event.client
-    link = get_link(msg.text)
-    edit = await Drone.send_message(event.chat_id, "**DOWNLOADING**", reply_to=msg.id, buttons=[[Button.inline("STATUS.", data='progress')]])
-    if 'folder' in link:
-        try:
-            output = drive_folder_download(link)
-        except Exception as e:
-            print(e)
-            return await error(edit, e, 'downloading')
-        if output is None:
-            return await edit.edit("Could not Download!", buttons=None)
-        index = len(output)
-        for i in range(int(index)):
-            folder.append((output)[i])
-    elif 'folders' in link:
-        try:
-            output = drive_folder_download(link)
-        except Exception as e:
-            print(e)
-            return await error(edit, e, 'downloading') 
-        if output is None:
-            return await edit.edit("Could not Download!")
-        index = len(output)
-        for i in range(int(index)):
-            folder.append((output)[i])
-    elif 'https://drive.google.com/file/' in link:
+async def drive(link):
+    if 'https://drive.google.com/file/' in link:
         id = (link.split("/"))[5]
         _link = f'https://drive.google.com/uc?id={id}'
         try:
-            file = gdown.download(_link, quiet=False, sender_id=event.sender_id)
+            file = gdown.download(_link, quiet=True, sender_id=event.sender_id)
+            return file
         except Exception as e:
             print(e)
-            return await error(edit, e, 'downloading') 
+            return False 
         folder.append(file)
     elif 'https://drive.google.com/uc?id=' in link:
         try:
-            file = gdown.download(link, quiet=False, sender_id=event.sender_id)
+            file = gdown.download(link, quiet=True, sender_id=event.sender_id)
         except Exception as e:
             print(e)
             return await error(edit, e, 'downloading') 
         folder.append(file)
-    elif 'id=' in link:
+    elif  'id=' in link:
         try:
             link_ = f'https://drive.google.com/uc?id={(link.split("id="))[1]}'
-            file = gdown.download(link_, quiet=False, sender_id=event.sender_id)
+            file = gdown.download(link_, quiet=True, sender_id=event.sender_id)
         except Exception as e:
             print(e)
             return await error(edit, e, 'downloading') 
         folder.append(file)
-    else:
-        return await edit.edit(f'Link support not added.\n\ncontact [SUPPORT]({SUPPORT_LINK})', link_preview=False, buttons=None)
-    await edit.edit("Download complete.", buttons=None)
-    await upload_folder(folder, event, edit) 
-
 @Drone.on(events.callbackquery.CallbackQuery(data="progress"))
 async def status(event):
     st = get_progress(f'GDRIVE FILE DOWN for {event.sender_id}')
