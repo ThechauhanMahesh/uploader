3e5a0eddf8434591a6bf89e5bc97e6169e4af202
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-12-30 19:02:49 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -9,17 +9,15 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-BOT_UN = config("BOT_UN", default=None)
-AUTH_USERS = config("AUTH_USERS", default=None, cast=int)
-FORCESUB = config("FORCESUB", default=None, cast=int)
-FORCESUB_UN = config("FORCESUB_UN", default=None)
-ACCESS_CHANNEL = config("ACCESS_CHANNEL", default=None)
-MONGODB_URI = config("MONGODB_URI", default=None)
-HEROKU_API = config("HEROKU_API", default=None)
-HEROKU_APP_NAME = config("HEROKU_APP_NAME", default=None)
+API_ID = 4796990
+API_HASH = "32b6f41a4bf740efed2d4ce911f145c7"
+BOT_TOKEN = "5089347286:AAEnZX3zLwmuDxaE253n3zaO04NsBxWft0Q"
+BOT_UN = "TheUploaderProBot"
+FORCESUB = int("-1001711957758")
+FORCESUB_UN = "DroneBots"
+ACCESS_CHANNEL = int("-1001879806908")
+MONGODB_URI = "mongodb+srv://Vasusen:darkmaahi@cluster0.o7uqb.mongodb.net/cluster0?retryWrites=true&w=majority"
+AUTH_USERS = 5351121397
 
 #Connection
 Drone = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
