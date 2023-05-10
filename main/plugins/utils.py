7c2772fe0498898a775e188f4c73c2cb6bfdab9c
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2023-05-10 20:27:06 +0530

    Update utils.py

--- main/plugins/utils.py
+++ main/plugins/utils.py
@@ -1,9 +1,25 @@
-
+import re
 from telethon import errors, events
 
+#regex-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#to get the url from event
+
+def get_link(string):
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(regex,string)   
+    try:
+        link = [x[0] for x in url][0]
+        if link:
+            return link
+        else:
+            return False
+    except Exception:
+        return False
+    
 # Forcesub
 from telethon.errors.rpcerrorlist import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
+
 
 async def force_sub(id):
     try:
