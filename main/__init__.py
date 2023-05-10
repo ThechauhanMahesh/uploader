7ddfea16221745f3fe695cb04d827619f9d638bb
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2023-05-10 20:38:43 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -11,11 +11,11 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 # variables
 API_ID = 4796990
 API_HASH = "32b6f41a4bf740efed2d4ce911f145c7"
-BOT_TOKEN = "5089347286:AAEnZX3zLwmuDxaE253n3zaO04NsBxWft0Q"
+BOT_TOKEN = "5089347286:AAGTwtxrFMSucuQdwLhfC7FvFXT_RfgxUuo"
 BOT_UN = "TheUploaderProBot"
-FORCESUB = int("-1001711957758")
+FORCESUB = int("-1001581768849")
 FORCESUB_UN = "DroneBots"
-ACCESS_CHANNEL = int("-1001879806908")
+ACCESS_CHANNEL = int("-1001768362393")
 MONGODB_URI = "mongodb+srv://Vasusen:darkmaahi@cluster0.o7uqb.mongodb.net/cluster0?retryWrites=true&w=majority"
 AUTH_USERS = 5351121397
 
